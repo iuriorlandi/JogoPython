4547815984bb75b20515f6e1a29c88b859287c70
Iuri <iuri.orlandi20@gmail.com>
2021-08-07 18:19:37 -0300

    adicionando rodadas

--- JogoPython.py
+++ JogoPython.py
@@ -3,15 +3,28 @@ def EscreveCabecalho():
     print("Jogo de adivinhação.")
     print("********************")
 
+
 EscreveCabecalho()
 
+total_tentativas = 3
 numero = 52
 
-numero_digitado = int(input())
+for rodada in range(1, total_tentativas + 1):
+    print("Rodada {} de {}".format(rodada, total_tentativas))
 
-if(numero == numero_digitado):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+    chute = int(input("Digite um número entre 1 e 100: "))
+    chute_valido = 0 < chute <= 100
+    acertou = chute == numero
+    maior = chute > numero
 
-input()
+    if not chute_valido:
+        print("Você deve chutar um número entre 1 e 100")
+        continue
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    elif (maior):
+        print("O número que você chutou é maior do que o número secreto!")
+    else:
+        print("O número que você chutou é menor do que o número secreto!")
